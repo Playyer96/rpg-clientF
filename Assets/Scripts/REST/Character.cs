a38aa578254ce2acabaab287dca689a601743a6f
Danilo <danilo.vanegas@upb.edu.co>
2017-05-26 22:36:20 -0500

    rpg-clientFinal

---
+++ Assets/Scripts/REST/Character.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Services;
+using UnityEngine;
+using UnityRest;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+[Serializable]
+[Description("character")]
+public class Character
+{
+    public int[] ids;
+    public List<ObjectId> charactersAvailable = new List<ObjectId>();
+    public static Character actualCharacter;
+
+    public Character(int[] ids)
+    {
+        this.ids = ids;
+    }
+
+    private void AddCharacters()
+    {
+        foreach (int id in ids)
+        {
+            charactersAvailable.Add(id.ToString());
+        }
+    }
+
+    public string Print()
+    {
+
+        AddCharacters();
+
+        string print = "";
+
+        for (int i = 0; i < ids.Length; i++)
+        {
+            if (i == 0)
+            {
+                print = string.Format("{0}", ids[i]);
+            }
+            else
+            {
+                print = string.Format("{0}, {1}", print, ids[i]);
+            }
+        }
+        return print;
+    }
+}
